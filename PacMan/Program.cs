64c0c8bd5b03eace906251e4b1ef23793d188b88
DIM1924 <90838782+DIM1924@users.noreply.github.com>
2021-11-25 14:15:40 +0300

    PacMan project start

--- PacMan/Program.cs
+++ PacMan/Program.cs
@@ -1,23 +1,22 @@
-using System;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
 using System.Windows.Forms;
 
-
-namespace WWW
+namespace PacMan
 {
     static class Program
     {
         /// <summary>
-        ///  The main entry point for the application.
+        /// Главная точка входа для приложения.
         /// </summary>
         [STAThread]
         static void Main()
         {
-            Application.SetHighDpiMode(HighDpiMode.SystemAware);
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            var engine = new Engine();
-            Application.Run(new Telega(engine));
+            Application.Run(new Form1());
         }
-
     }
 }
